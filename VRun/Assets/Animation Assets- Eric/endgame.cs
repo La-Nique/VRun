e072293c67d1b90cd8627fd3b9e3b428f06161bb
La-Nique <54916166+La-Nique@users.noreply.github.com>
2022-07-10 03:53:19 -0400

    endgame: relocated ship, added c# script, fixed
    trees and leveled out ground so easier navigation, added a "training room", added a rock >__> , made surrounding land elvated so user cannot escape. GG

---
+++ VRun/Assets/Animation Assets- Eric/endgame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endgame : MonoBehaviour
+{
+    // Alien Ship drag onto another object. Make alien ship a child of another object...
+    
+    // Within a certain distance
+    // 3D distance formula (x,y,z) or 2D (x,z)
+    // range of these coordinates around the ship 
+    // go to end screen.
+    // if (x,y) character position then go to end screen.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

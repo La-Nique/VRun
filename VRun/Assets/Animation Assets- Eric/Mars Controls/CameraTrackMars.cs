529b3be1931ed8b0491d2fd5b3dd1a355a06b3ac
Eric Dittus <ericdittus@Erics-Air.hitronhub.home>
2022-07-11 21:48:26 -0400

    did camera

--- VRun/Assets/Animation Assets- Eric/Mars Controls/CameraTrackMars.cs
+++ VRun/Assets/Animation Assets- Eric/Mars Controls/CameraTrackMars.cs
@@ -7,7 +7,8 @@ public class CameraTrackMars : MonoBehaviour
     public Transform charPosition;
     //public float distance = 1.0f;
     public float offset = 20f;
-    public float delay = 0.02f;
+    public float delay = 0.01f;
+    public float distance = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,7 @@ public class CameraTrackMars : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 follow = charPosition.position - charPosition.forward;
+        Vector3 follow = charPosition.position - charPosition.forward * distance;
         follow.y += offset;
         transform.position +=(follow - transform.position) * delay;
         transform.LookAt(charPosition.transform);
